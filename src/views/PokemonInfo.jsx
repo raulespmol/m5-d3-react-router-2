6a43116d6f9b79241fc6bc9e08c22c533fd2adec
Raul Espinoza M <raul.espmol@gmail.com>
2024-02-29 19:01:47 -0300

    crear poke info

--- src/views/PokemonInfo.jsx
+++ src/views/PokemonInfo.jsx
@@ -1,16 +1,30 @@
-import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
 
 
 const PokemonInfo = () => {
+  const [PokeInfo, setPokeInfo] = useState({})
   const navigate = useNavigate()
+  const { id } = useParams()
 
   const handleClick = () => {
     navigate('/pokemons')
   }
 
+  const getPokeInfo = () => {
+    const url = `https://pokeapi.co/api/v2/pokemon-species/${id}`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => setPokeInfo(data))
+  }
+
+  useEffect(() => {
+    getPokeInfo
+  })
+
   return (
     <>
-      <div>PokemonInfo</div>
+      <div>PokemonInfo {id}</div>
       <button onClick={handleClick}>Volver</button>
     </>
   )
