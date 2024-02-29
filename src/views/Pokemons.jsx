632b41cbd56dd173e1f99a00f36a0a2fbbb5dda5
Raul Espinoza M <raul.espmol@gmail.com>
2024-02-29 17:30:50 -0300

    map options

--- src/views/Pokemons.jsx
+++ src/views/Pokemons.jsx
@@ -1,6 +1,19 @@
+import { useContext } from "react"
+import { PokeContext } from "../context/PokeContext"
+
 const Pokemons = () => {
+  const {pokes} = useContext(PokeContext)
+
   return (
-    <div>Pokemons</div>
+    <div>
+      <select>
+        {pokes.map( poke => {
+          return (
+            <option value={poke.entry_number}>{poke.pokemon_species.name}</option>
+          )
+        } )}
+      </select>
+    </div>
   )
 }
 
